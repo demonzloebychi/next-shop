e9ab0acd413de2cfd00c482ebc647bcca0e82aa5
nikolas <nikolaislnkv@gmail.com>
2025-04-18 10:38:07 +0300

    upd cart icon and css

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -6,7 +6,12 @@ import { IProducts } from "@/app/products/products.interface";
 import CartContext from '@/app/contexts/CartContext';
 
 export default function AddToCartButton({ product }: { product: IProducts }) {
+  const [isInCard, setIsInCard] = useState(false)
   const cartContext = useContext(CartContext);
+
+
+
+
 
   if (!cartContext) {
     throw new Error('CartContext не найден');
@@ -14,11 +19,16 @@ export default function AddToCartButton({ product }: { product: IProducts }) {
 
   const handleAddToCart = () => {
     cartContext.handleAddToCart(product);
+    setIsInCard(true)
+
   };
 
   return (
-    <button className="card-button" onClick={handleAddToCart}>
-      В корзину
+    <button 
+      className="card-button" 
+      onClick={handleAddToCart}
+    >
+      {isInCard ? 'В корзине' : 'В корзину'}
     </button>
   );
 }
